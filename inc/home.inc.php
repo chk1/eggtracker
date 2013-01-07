b1b636f5ed241671d4a9a792ac6527da57e38f26
Christoph Kisfeld <mail@b-web.org>
2013-01-07 11:16:03 +0100

    Vektor-markers on map with Openlayers in WGS84

--- inc/home.inc.php
+++ inc/home.inc.php
@@ -4,10 +4,14 @@
 <div id="map"><script>init();</script></div>
 
 <script type="text/javascript">
-	pp = new OpenLayers.LonLat(7,52);
+	//var point = new OpenLayers.Geometry.Point(350000, 5970000);
+	var point = new OpenLayers.Geometry.Point(7, 52);
+
+	var wgs84 = new OpenLayers.Projection("EPSG:4326"); // WGS84
+	var osm_sphm = new OpenLayers.Projection("EPSG:3857"); // OSM Spherical Mercator
+	point.transform(wgs84, osm_sphm);
 
 	vectors = new OpenLayers.Layer.Vector("Vector Layer");
-	point = new OpenLayers.Geometry.Point(pp.lon, pp.lat); // immer 0,0??
 	vectors.addFeatures([new OpenLayers.Feature.Vector(point)]);
 	map.addLayer(vectors); 
 </script>
