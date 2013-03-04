66c8d4630cff2e1d6401783cf6d58e8c3d17b8f1
Christoph Kisfeld <mail@b-web.org>
2013-03-04 11:16:03 +0100

    Code cleanup

--- cron/fetch_historic_data.php
+++ cron/fetch_historic_data.php
@@ -10,32 +10,63 @@ $opts = array(
   )
 );
 
-/*
-available datastreams for Air Quality Eggs in the Cosm API:
-	button
-	CO
-	humidity
-	NO2
-	O3
-	temperature
-*/
-$streams = array("CO", "humidity", "NO2", "O3", "temperature"); // same as database tables
-
 $dbconn = pg_connect("host=". $conf["db"]["host"] .
 					" port=". $conf["db"]["port"] . 
 					" dbname=". $conf["db"]["db"] .
 					" user=". $conf["db"]["user"] .
 					" password=". $conf["db"]["pass"]);
 
+// $eggid   cosm egg id
+// $stream  datastream, like CO, temperature...
+// $start   start date
+function fetchJsonFromCosm($eggid, $stream, $start = "2012-11-01T00:00:01") {
+	global $opts;
+	$end = date("Y-m-d\TH:i:s", strtotime($start)+(3600*24)); // +1 day
+	$url = "http://api.cosm.com/v2/feeds/".$eggid."/datastreams/".$stream.".json?start=".urlencode($start)."&end=".urlencode($end)."&interval=60";
+	$f = file_get_contents($url, false, stream_context_create($opts));
+	$json = json_decode($f, true);
+	return $json;
+}
+
+function insertIntoDatabase($eggid, $stream, $data_value, $data_datetime) {
+	global $dbconn;
+	// use "upsert" technique to INSERT only when no duplicate key exists and thereby eliminating script errors
+	$insert1 = pg_query($dbconn, "UPDATE {$stream} SET {$stream}={$data_value} WHERE eggid='{$eggid}' AND time = '{$data_datetime}';");
+	$insert2 = pg_query($dbconn, "INSERT INTO {$stream} (eggid, time, {$stream}) 
+				SELECT {$eggid}, '{$data_datetime}', '{$data_value}'
+				WHERE NOT EXISTS 
+				(SELECT 1 FROM {$stream} WHERE eggid = '{$eggid}' AND time = '{$data_datetime}');");
+	if(!$insert1 || !$insert2) {
+		echo "Database error: ".pg_last_error()."".PHP_EOL;
+	} else {
+		echo "Inserted measurement :";
+		echo $data_datetime.": ".$data_value."<br>".PHP_EOL;
+		return true;
+	}
+	return false;
+}
+
+
+/*
+	For every stream and egg, we are going to query the Cosm API for data.
+	If data is found, insert it into our own database.
+
+	Available datastreams for Air Quality Eggs in the Cosm API:
+		button, CO, humidity, NO2, O3, temperature
+*/
+$streams = array("CO", "humidity", "NO2", "O3", "temperature"); // same as database tables
 foreach ($streams as $stream) {
 	echo "<h2>".$stream."</h2>";
 
+	// query eggs
 	$result = pg_query($dbconn, 'SELECT cosmid, eggid FROM eggs WHERE active = true;');
 	if(!$result) { die('SQL Error'); }
-	while($row = pg_fetch_assoc($result)) {
 
+	// iterate eggs
+	while($row = pg_fetch_assoc($result)) {
 		echo "<h3>".$row['cosmid']."</h3>";
 
+		// find the latest insertion for egg and datastream
 		$result1 = pg_query($dbconn, "SELECT MAX(time) as last_entry_date FROM {$stream} WHERE eggid = '{$row['cosmid']}';");
 		if(!$result1) { die('SQL Error'); }
 		$row1 = pg_fetch_assoc($result1);
@@ -44,7 +75,6 @@ foreach ($streams as $stream) {
 		// end is always set to 1 day from start
 		if($row1["last_entry_date"] == "") {
 			// if there are no rows in the database, start our data collection at November first
-
 			// continuus data fetching since 2012-11-01
 			echo "No entries for ".$stream." yet.<br>".PHP_EOL;
 			$start = "2012-11-01T00:00:01";
@@ -55,21 +85,17 @@ foreach ($streams as $stream) {
 			$end   = date("Y-m-d\TH:i:s", strtotime($row1["last_entry_date"])+3600*24); // +1 day
 		};
 
-		$url = "http://api.cosm.com/v2/feeds/".$row["cosmid"]."/datastreams/".$stream.".json?start=".urlencode($start)."&end=".urlencode($end)."&interval=60";
-		echo $url."<br>";
-		echo urldecode($url)."<br>";
-		if($f = file_get_contents($url, false, stream_context_create($opts))) {
-			$streamjson = json_decode($f, true);
+		if($cosmdata = fetchJsonFromCosm($row["cosmid"], $stream, $start)) {
 			echo "JSON Download: <b>".$start."</b> - <b>".$end."</b><br>".PHP_EOL;
 			
-			if(!isset($streamjson['datapoints'])) {
-				
+			if(!isset($cosmdata['datapoints'])) {
+
 				// if there are no datapoints and it's not "today",
 				// then there's likely a data gap = no data from cosm for this day
 
 				// guess where the next data could be by querying every day until today, or until data found
 				$i=0;
-				while(!isset($streamjson2['datapoints'])) {
+				while(!isset($cosmdata_['datapoints'])) {
 					$newstart = date("Y-m-d\TH:i:s", strtotime($start)+(3600*24)*$i);
 					$newend   = date("Y-m-d\TH:i:s", strtotime($newstart)+(3600*24)); // +1 day
 					echo $i.": ".$newstart." - ".$newend."<br>";
@@ -77,29 +103,14 @@ foreach ($streams as $stream) {
 					flush();
 					if(strtotime($newstart) > time()) break;
 
-					$url = "http://api.cosm.com/v2/feeds/".$row["cosmid"]."/datastreams/".$stream.".json?start=".urlencode($newstart)."&end=".urlencode($newend)."&interval=60";
-					$f = file_get_contents($url, false, stream_context_create($opts));
-					$streamjson2 = json_decode($f, true);
+					$cosmdata_ = fetchJsonFromCosm($row["cosmid"], $stream, $newstart);
 				}
-				$streamjson = $streamjson2;
+				$cosmdata = $cosmdata_;
 
 			}
 
-			foreach($streamjson['datapoints'] as $datapoint) {
-				// use "upsert" technique to INSERT when no duplicate key exists
-				$ins1 = pg_query($dbconn, "UPDATE {$stream} SET {$stream}={$datapoint['value']} WHERE eggid='{$row['cosmid']}' AND time = '{$datapoint['at']}';");
-				$ins2 = pg_query($dbconn, "INSERT INTO {$stream} (eggid, time, {$stream}) 
-							SELECT {$row['cosmid']}, '{$datapoint['at']}', '{$datapoint['value']}'
-							WHERE NOT EXISTS 
-							(SELECT 1 FROM {$stream} WHERE eggid = '{$row['cosmid']}' AND time = '{$datapoint['at']}');");
-				if(!$ins1 || !$ins2) {
-					echo "Database error: ".pg_last_error()."".PHP_EOL;
-					flush();
-				} else {
-					echo "Inserted measurement :";
-					echo $datapoint['at'].": ".$datapoint['value']."<br>".PHP_EOL;
-					flush();
-				}
+			foreach($cosmdata['datapoints'] as $datapoint) {
+				insertIntoDatabase($row['cosmid'], $stream, $datapoint['value'], $datapoint['at']);
 			}
 			
 		} else {
