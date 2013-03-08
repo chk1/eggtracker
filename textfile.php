26dbbdf7e1d61a407cccc53ed80e380a0bfa844a
Christoph Kisfeld <mail@b-web.org>
2013-03-08 13:43:41 +0100

    Better marker popups

--- textfile.php
+++ textfile.php
@@ -12,21 +12,23 @@ if(!$result) { die('SQL Error'); }
 while($row = pg_fetch_assoc($result)) {
 	echo $row['y'].",";
 	echo $row['x']."\t";
-	echo $row['cosmid']."\t";
+	echo '<a href="http://cosm.com/feeds/'.$row['cosmid'].'">Egg '.$row['cosmid']."</a>"."\t";
 	echo "<table>";
 		
-		$data = pg_query($dbconn, 'SELECT time, CO FROM CO ORDER BY time DESC LIMIT 3 ');
-		while($row_ = pg_fetch_assoc($data)) {
+
+		$streams = array("CO", "humidity", "NO2", "O3", "temperature"); // same as database tables
+
+		foreach($streams as $stream) {
+			$result_ = pg_query($dbconn, "SELECT time, {$stream} FROM {$stream} ORDER BY time DESC LIMIT 1 ");
+			$row_ = pg_fetch_assoc($result_);
+
 			echo "<tr>";
-				echo "<td>Zeit</td>";
-				echo "<td>CO</td>";
-			echo "</tr>";
-			echo "<tr>";
-				echo "<td>".date("d.m.Y H:i:s", strtotime($row_["time"]))."</td>";
-				echo "<td>".$row_["co"]."</td>";
+			echo "<td>$stream</td>";
+			//$tb .= "<td>".date("d.m.Y H:i:s", strtotime($row_["time"]))."</td>";
+			echo "<td>".$row_[strtolower($stream)]."</td>";
 			echo "</tr>";
 		}
-		
+
 	echo "</table>"."\t";
 	echo "./img/eggicon.png";
 	echo PHP_EOL;
