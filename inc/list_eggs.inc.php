09d17e230f9d8418228ac3b25085386ef80604ed
froehr <f.roehr@uni-muenster.de>
2013-03-25 14:41:52 +0100

    Links zu cosm.com und Lanuv hinzugefügt

--- inc/list_eggs.inc.php
+++ inc/list_eggs.inc.php
@@ -1,16 +1,56 @@
 <p>
 <?php
-require_once("inc/config.inc.php");
+include("../inc/config.inc.sample.php");
 $dbconn = pg_connect("host=". $conf["db"]["host"] .
 					" port=". $conf["db"]["port"] . 
 					" dbname=". $conf["db"]["db"] .
 					" user=". $conf["db"]["user"] .
 					" password=". $conf["db"]["pass"]);
-$result = pg_query($dbconn, 'SELECT eggid, cosmid, ST_Y(geom) as y, ST_X(geom) as x FROM eggs WHERE active = true');
+
+$tabelle = "<table border=\"0\">
+  <tr>
+  	<th>Eggtracker ID</th>
+    <th>Cosm.com ID</th>
+    <th>Geographische Koordinaten</th>
+  </tr>";
+
+$result = pg_query($dbconn, 'SELECT eggid, cosmid, ST_Y(geom) as y, ST_X(geom) as x FROM eggs WHERE active = true AND eggid < 1000000');
 if(!$result) { die('SQL Error'); }
 while($row = pg_fetch_assoc($result)) {
-	echo "#". $row['eggid'] .", #". $row['cosmid'] ." @ (". $row['x'] ."|". $row['y'] .")<br>";
+	$tabelle .= "<tr>".'<td align="center" valign="middle">'.$row['eggid']."</td>".'<td align="center" valign="middle">'.'<a href="https://cosm.com/feeds/'.$row["cosmid"].'">'.$row['cosmid']."</a>"."</td>".'<td align="center" valign="middle">'.$row['y'] ."  ". $row['x']."</td>"."</tr>";
 }
+$tabelle.= "</table>";
+echo "Air Quality Eggs:";
+echo $tabelle;
+
+$tabelle = "<table border=\"0\">
+  <tr>
+  	<th>Eggtracker ID</th>
+    <th>LANUV-Referenz</th>
+    <th>Geographische Koordinaten</th>
+  </tr>";
+
+$Messstationen_array[1][1] = "http://www.lanuv.nrw.de/luft/messorte/steckbriefe/vms2.htm";
+$Messstationen_array[1][2] = "M&uumlnster, Weseler Str.";
+$Messstationen_array[2][1] = "http://www.lanuv.nrw.de/luft/messorte/steckbriefe/msge.htm";
+$Messstationen_array[2][2] = "M&uumlnster-Geist";
+
+$o = 1;
+
+$result = pg_query($dbconn, 'SELECT eggid, cosmid, ST_Y(geom) as y, ST_X(geom) as x FROM eggs WHERE active = true AND eggid < 1000000');
+if(!$result) { die('SQL Error'); }
+while($row = pg_fetch_assoc($result)) {
+	$tabelle .= "<tr>".'<td align="center" valign="middle">'.$row['eggid']."</td>".'<td align="center" valign="middle">'."<a href=".$Messstationen_array[$o][1].">".$Messstationen_array[$o][2]."</a>"."</td>".'<td align="center" valign="middle">'.$row['y'] ."  ". $row['x']."</td>"."</tr>";
+	$o ++;
+}
+$tabelle.= "</table>";
+
+echo "</br>";
+echo "</br>";
+echo "</br>";
+echo "LANUV Messstationen:";
+echo $tabelle;
+
 
 pg_close($dbconn);
 ?>
