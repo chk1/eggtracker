b535a2f144c63cc85b749f1db1ce84ee6459d619
ChAbe <ch.abe@wwu.de>
2013-04-09 17:50:12 +0300

    image fix

--- inc/more.inc.php
+++ inc/more.inc.php
@@ -1,14 +1,14 @@
 <br>
 <div class="tabledownload" ><table>
   <tr>
-    <th><a href="?action=export_form "><img src="/img/daten_export.png"/></a></th>
+    <th><a href="?action=export_form "><img src="/eggtracker/img/daten_export.png"/></a></th>
     <td>Hier können Sie sich die gesammelten Daten in folgenden formaten Downloaden und weiterverwenden: 
       XML,
       CSV,
       Json<br></td>
     </tr>
   <tr>
-    <td><a href="?action=list_values"><img src="/img/tabellen_ansicht.png" /></a></td>
+    <td><a href="?action=list_values"><img src="/eggtracker/img/tabellen_ansicht.png" /></a></td>
     <td>Es können alle gesammelten Umweltdatendaten für einen von Ihnen ausgewähltem Zeitraum, in einer Tabelle angezeigt werden.</td>
     </tr>
 </table>
