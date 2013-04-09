eff2865fb7f45ed21525bfd90bc6349899e5a94a
ChAbe <ch.abe@wwu.de>
2013-04-09 17:39:33 +0300

    image and table update

--- inc/more.inc.php
+++ inc/more.inc.php
@@ -1,17 +1,16 @@
-<table width="500" border="1">
+<br>
+<div class="tabledownload" ><table>
   <tr>
-    <td><a href="?action=export_form">Daten Download</a></td>
-  </tr>
+    <th><a href="?action=export_form "><img src="../img/daten_export.png" alt="" /></a></th>
+    <td><a href="?action=export_form "></a>Hier können Sie sich die gesammelten Daten in folgenden formaten Downloaden und weiterverwenden: 
+      XML,
+      CSV,
+      Json</td>
+    </tr>
   <tr>
-    <td>Hier können Sie sich die gesammelten Daten in folgenden formaten Downloaden und weiterverwenden:<br>
-XML<br>
-CSV<br>
-Json</td>
-  </tr>
-  <tr>
-    <td><a href="?action=list_values">Daten Anzeigen</a></td>
-  </tr>
-  <tr>
-    <td>Es können alle gesammelten Umweltdatendaten für einen von Ihnen ausgewähltem Zeitraum, in einer Tabelle angezeigt werden.</td>
-  </tr>
+    <td><a href="?action=list_values"><img src="../img/tabellen_ansicht.png" alt="" /></a></td>
+    <td><a href="?action=list_values"></a>Es können alle gesammelten Umweltdatendaten für einen von Ihnen ausgewähltem Zeitraum, in einer Tabelle angezeigt werden.</td>
+    </tr>
 </table>
+
+</div>
