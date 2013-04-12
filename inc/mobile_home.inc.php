93de9026573d05a047ffc8899052213e56528a7f
froehr <f.roehr@uni-muenster.de>
2013-04-12 15:48:43 +0200

    Link fix

--- inc/mobile_home.inc.php
+++ inc/mobile_home.inc.php
@@ -1,11 +1,11 @@
 <p>
-<a href="./?action="><img src="/eggtracker/img/mobile_mapview.png" /></a><br><br>
+<a href="./?action="><img src="/eggtracker/img/mobile_mapview.png" /></a><br>
 
-<a href="./?action=diagram"><img src="/eggtracker/img/mobile_mapview.png"/></a><br><br>
+<a href="./?action=diagram"><img src="/eggtracker/img/mobile_chartview.png"/></a><br>
 
-<a href="./?action=list"><img src="/eggtracker/img/mobile_egglist.png"/></a><br><br>
+<a href="./?action=list"><img src="/eggtracker/img/mobile_egglist.png"/></a><br>
 
-<a href="./?action=about"><img src="/eggtracker/img/mobile_about.png"/></a><br><br>
+<a href="./?action=about"><img src="/eggtracker/img/mobile_about.png"/></a><br>
 
-<a href="./?action=contact"><img src="/eggtracker/img/mobile_contact.png"/></a><br><br>
+<a href="./?action=contact"><img src="/eggtracker/img/mobile_contact.png"/></a><br>
 </p>
