13150d60d44dad5580bee2c3e2f75ce63e060758
ChAbe <ch.abe@wwu.de>
2013-04-12 14:56:43 +0300

    Update mobile_home.inc.php

--- inc/mobile_home.inc.php
+++ inc/mobile_home.inc.php
@@ -4,4 +4,5 @@
 <a href="./?action=list">Ei Liste</a><br><br>
 <a href="./?action=about">Über diese Seite</a><br><br>
 <a href="./?action=contact">Kontakt</a><br><br>
+<a href="./?action=list_values">Werte Anzeigen</a><br><br>
 </p>
