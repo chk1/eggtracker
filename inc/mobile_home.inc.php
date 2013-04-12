97fb0314e7ba8d0a8641b739072afabef79e7611
ChAbe <ch.abe@wwu.de>
2013-04-12 15:33:36 +0300

    Update mobile_home.inc.php

--- inc/mobile_home.inc.php
+++ inc/mobile_home.inc.php
@@ -1,7 +1,9 @@
 <p>
-<a href="./?action=">Zurück zur Karte</a><br><br>
+<a href="./?action="><img src="/eggtracker/img/kartenansicht.png" /></a>
+<!--<a href=>Zurück zur Karte</a><br><br>-->
 <a href="./?action=diagram">Diagramme</a><br><br>
 <a href="./?action=list">Ei Liste</a><br><br>
 <a href="./?action=about">Über diese Seite</a><br><br>
 <a href="./?action=contact">Kontakt</a><br><br>
+
 </p>
