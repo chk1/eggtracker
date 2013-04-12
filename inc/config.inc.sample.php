69c4d949ab00de8e92bdfef958bcb514282a086c
Christoph Kisfeld <spam@b-web.org>
2013-04-12 13:41:38 +0200

    Contact form and email in config

--- inc/config.inc.sample.php
+++ inc/config.inc.sample.php
@@ -1,5 +1,10 @@
 <?php
-$conf["apikey"] = "1234abcd"; // COSM API Key
+// your email address
+$conf["email"] = "you@example.com";
+// your Cosm api key
+$conf["apikey"] = "1234abcd";
+
+// database configuration
 $conf["db"]["host"] = "localhost";
 $conf["db"]["port"] = 5432;
 $conf["db"]["user"] = "";
@@ -8,6 +13,7 @@ $conf["db"]["pass"] = "";
 
 // Specify the center of your map here
 // This affects the map view and search radius on Cosm
+// for Air Quality Egg searches
 $conf["location"]["lat"] = "51.963519";
 $conf["location"]["lon"] = "7.613268";
 ?>
