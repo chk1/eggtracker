89966a73db9c304023ab0faf5fd7663a622f7806
ChAbe <ch.abe@wwu.de>
2013-04-14 14:38:40 +0300

    Update mobile_home.inc.php

--- inc/mobile_home.inc.php
+++ inc/mobile_home.inc.php
@@ -1,9 +1,8 @@
-<div align="center"><a href="./?action="><img src="/eggtracker/img/mobile_mapview.png" /></a><br>
-  
-  <a href="./?action=diagram"><img src="/eggtracker/img/mobile_chartview.png"/></a><br>
-  
-  <a href="./?action=list"><img src="/eggtracker/img/mobile_egglist.png"/></a><br>
-  
-  <a href="./?action=about"><img src="/eggtracker/img/mobile_about.png"/></a><br>
-  
-  <a href="./?action=contact"><img src="/eggtracker/img/mobile_contact.png"/></a></div>
+<div align="center">
+<br>
+  <a href="./?action="><img src="/eggtracker/img/mobile_mapview.png" /></a> <br>  
+  <a href="./?action=diagram"><img src="/eggtracker/img/mobile_chartview.png"/></a> <br>
+  <a href="./?action=list"><img src="/eggtracker/img/mobile_egglist.png"/></a> <br>
+  <a href="./?action=about"><img src="/eggtracker/img/mobile_about.png"/></a> <br>
+  <a href="./?action=contact"><img src="/eggtracker/img/mobile_contact.png"/></a>
+ </div>
