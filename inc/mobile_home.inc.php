6504794f81d7e215669afd07cdc84211971b4e93
froehr <f.roehr@uni-muenster.de>
2013-04-14 19:01:34 +0200

    Anpassung der mobilen Navigation

--- inc/mobile_home.inc.php
+++ inc/mobile_home.inc.php
@@ -3,6 +3,7 @@
 	<a href="./?action="><img src="/eggtracker/img/mobile_mapview.png" /></a> <br>  
 	<a href="./?action=diagram"><img src="/eggtracker/img/mobile_chartview.png"/></a> <br>
 	<a href="./?action=list"><img src="/eggtracker/img/mobile_egglist.png"/></a> <br>
+	<a href="./?action=list_values"><img src="/eggtracker/img/mobile_tableview.png"/></a> <br>
 	<a href="./?action=about"><img src="/eggtracker/img/mobile_about.png"/></a> <br>
 	<a href="./?action=contact"><img src="/eggtracker/img/mobile_contact.png"/></a>
 	<br><br>
